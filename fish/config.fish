523aa14bc9e7204d11847aa47bef284c797dfda3
Kevin D <hello@kevind.me>
2022-02-10 09:04:25 -0500

    Add aliases

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+alias "vim"="nvim"
+alias ".."="cd .."
+
 function fish_greeting
   figlet -w 100 kevind.me 
 end
